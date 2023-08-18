e31143e3c3a9cacbcdac3900b634b3e594546cda
iluizxxs <luiz.silva34@estudante.ifms.edu.br>
2023-08-18 14:25:58 -0400

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import {Routes, Route, BrowserRouter} from "react-router-dom";
-
-import App from './app.jsx'
+import Home from "./Home/Home.jsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
-
-    <Route path='/app' element= {<App/>}></Route>
-
-    </Routes>
-    </BrowserRouter>
+ <Route path="/home" element={<Home/>} ></Route>
+ <Route path="/todo" element={<Todo/>} ></Route>
+  </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 )
